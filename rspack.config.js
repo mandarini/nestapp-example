1701415d1147dd5b209e371d795b102e56dc9660
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-04-04 18:19:56 +0300

    removed some dependencies and fixed rspack config

--- rspack.config.js
+++ rspack.config.js
@@ -1,21 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
-module.exports = (env,argv) =>  {
-  // console.log('env:',env)
-  // console.log('argv:',argv)
+module.exports = (env, argv) => {
   return {
+    externals: {
+      '@nestjs/common': '"@nestjs/common"',
+      '@nestjs/core': '"@nestjs/core"',
+      tslib: '"tslib"',
+    },
+    externalsPresets: { node: true },
     context: __dirname,
     entry: {
-        main: './dist/main.js'
+      main: './dist/main.js',
     },
     output: {
-        path: path.resolve(__dirname, 'dist')
+      path: path.resolve(__dirname, 'dist'),
     },
     infrastructureLogging: {
-      debug: true
+      debug: false,
     },
-    // target: "node",
-    // devServer: {
-    //   hot: true
-    // },
-  }
-}
+    target: 'node',
+  };
+};
